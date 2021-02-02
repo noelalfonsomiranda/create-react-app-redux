e16b5f8a5cf817e38dcded291a888f253c1cb830
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2021-02-02 18:46:11 +0800

    modify actions

--- src/containers/ToDo/actions.js
+++ src/containers/ToDo/actions.js
@@ -1,15 +1,8 @@
-import {createActions} from 'redux-actions'
+import {createAction} from 'redux-actions'
 import {ADD_TODO, GET_TODO_LIST, RESULT_TODO_LIST} from './constants'
 
-const ReduxActions = createActions(
-  {},
-  ADD_TODO,
-  GET_TODO_LIST,
-  RESULT_TODO_LIST
-)
+const addTodo = createAction(ADD_TODO)
+const getTodoList = createAction(GET_TODO_LIST)
+const resultTodoList = createAction(RESULT_TODO_LIST)
 
-export const {addTodo, getTodoList, resultTodoList} = {
-  addTodo: ReduxActions['app/toDo/addTodo'],
-  getTodoList: ReduxActions['app/toDo/getTodoList'],
-  resultTodoList: ReduxActions['app/toDo/resultTodoList'],
-}
+export {addTodo, getTodoList, resultTodoList}
